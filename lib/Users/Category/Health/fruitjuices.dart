cb3cb93bfa0e37645d860dea41491bf415157294
swaadheenta <gayatriagnani7@gmail.com>
2021-03-30 10:40:25 +0530

    sub categories added in Beverages !!

---
+++ lib/Users/Category/Health/fruitjuices.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class FruitJuice extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/Users/Category/category.dart
+++ lib/Users/Category/category.dart
@@ -1,4 +1,11 @@
 import 'package:Online_grocery_app/Helpers/Devicesize.dart';
+import 'package:Online_grocery_app/Users/Category/Health/Allbeverages.dart';
+import 'package:Online_grocery_app/Users/Category/Health/coffee.dart';
+import 'package:Online_grocery_app/Users/Category/Health/energy.dart';
+import 'package:Online_grocery_app/Users/Category/Health/fruitjuices.dart';
+import 'package:Online_grocery_app/Users/Category/Health/health.dart';
+import 'package:Online_grocery_app/Users/Category/Health/tea.dart';
+import 'package:Online_grocery_app/Users/Category/Health/water.dart';
 import 'package:Online_grocery_app/Users/Category/basictile.dart';
 import 'package:Online_grocery_app/Users/Category/categorylist.dart';
 import 'package:flutter/material.dart';
@@ -31,29 +38,28 @@ class _CategoryState extends State<Category> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Shop By Category"),
-      ),
-        body: Stack(children: [
-      Positioned(
-        top: displayHeight(context)*0.02,
-          child: Container(
-            height: displayHeight(context)*0.78,
-            width: displayWidth(context),
-            //color: Colors.blue,
-                      child: ListView(
-            children:
-                Categorylist.map((tile) => BasicTileWidget(tile: tile)).toList(),
+        appBar: AppBar(
+          title: Text("Shop By Category"),
         ),
+        body: Stack(children: [
+          Positioned(
+            top: displayHeight(context) * 0.02,
+            child: Container(
+              height: displayHeight(context) * 0.78,
+              width: displayWidth(context),
+              child: ListView(
+                children:
+                    Categorylist.map((tile) => BasicTileWidget(tile: tile))
+                        .toList(),
+              ),
+            ),
           ),
-      ),
-    ]));
+        ]));
   }
 }
 
-class BasicTileWidget extends StatelessWidget {
+class BasicTileWidget extends StatefulWidget {
   final Mytile tile;
-
 
   const BasicTileWidget({
     Key key,
@@ -61,45 +67,71 @@ class BasicTileWidget extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _BasicTileWidgetState createState() => _BasicTileWidgetState();
+}
+
+class _BasicTileWidgetState extends State<BasicTileWidget> {
+  bool selected = false;
+  @override
   Widget build(BuildContext context) {
-    final title = tile.title;
-    final image=tile.image;
-    final tiles = tile.children;
-      Color color=Colors.grey;
+    final title = widget.tile.title;
+    final image = widget.tile.image;
+    final tiles = widget.tile.children;
+    Color color = Colors.grey;
+
     String name;
     if (tiles.isEmpty) {
       return ListTile(
           title: Text(title),
-          onTap: () => Scaffold.of(context)
-              .showSnackBar(SnackBar(content: Text(title))));
-    }
-    return Padding(
-      padding: const EdgeInsets.only(left:5.0 ,right: 5.0),
-      child: Card(
-        elevation: 10.0,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-        child: 
-       Padding(
-          padding: const EdgeInsets.only(left: 5.0, right: 5.0, top: 8.0),
-          child: Container(
+          onTap: () {
             
-          /*  decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(10.0)),
-              border: Border.all(color: Theme.of(context).hintColor),
-            ),*/
-           
-           // width: displayWidth(context) * 0.6,
-            child: ExpansionTile(
-              leading:Container(
-                //height: displayHeight(context)*0.06,
-                width: displayWidth(context)*0.2,
-                child: Image(image: AssetImage(image),fit: BoxFit.fill,)),
-              title: Text(title),
-              children: tiles.map((tile) => BasicTileWidget(tile: tile)).toList(),
+            if (title == "All Beverages")
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => All()));
+            else if (title == "Coffee")
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Cof()));
+            else if (title == "Energy & Soft drinks")
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Energy()));
+            else if (title == "Water")
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Water()));
+            else if (title == "Tea")
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Tea()));
+            else if (title == "Health Drink & Supplement")
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => Healthdrinks()));
+            else if (title == "Fruit Drinks & Juices")
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => FruitJuice()));
+          });
+    } else {
+      return Padding(
+        padding: const EdgeInsets.only(left: 5.0, right: 5.0),
+        child: Card(
+          elevation: 10.0,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 5.0, right: 5.0, top: 8.0),
+            child: Container(
+              child: ExpansionTile(
+                leading: Container(
+                    width: displayWidth(context) * 0.2,
+                    child: Image(
+                      image: AssetImage(image),
+                      fit: BoxFit.fill,
+                    )),
+                title: Text(title),
+                children:
+                    tiles.map((tile) => BasicTileWidget(tile: tile)).toList(),
+              ),
             ),
           ),
         ),
-      ),
-    );
+      );
+    }
   }
 }
